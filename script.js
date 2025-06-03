5e3d01fb68e74bc6cca103188525a13de3213613
Max-31 <nathrajarshi25@gmail.com>
2025-06-03 16:19:18 +0530

    ft.: initial commit

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 function openGmailCompose() {
     const email = "nathrajarshi25@gmail.com";
-    //const gmailURL = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}`;
     const gmailURL = `https://mail.google.com/mail/?view=cm&to=${email}`;
 
     window.open(gmailURL, "_blank");
 }
 
+function goToGithub(){
+    const githublURL = `https://github.com/Max-31`;
+
+    window.open(githublURL, "_blank");
+}
+
+function goToLinkedinCert(){
+    const linkedinCertlURL = `https://www.linkedin.com/in/rajarshi-nath-781456255/details/certifications/`;
+
+    window.open(linkedinCertlURL, "_blank");
+}
+
 // const downloadResume = async () => {
-async function downloadResume(){
-    try {
-        const response = await fetch(`http://localhost:3000/download/ResumeRajarshi.pdf`);
+// async function downloadResume(){
+//     try {
+//         const response = await fetch(`http://localhost:3000/download/ResumeRajarshi.pdf`);
 
-        if (!response.ok) {
-            throw new Error("Download failed!");
-        }
+//         if (!response.ok) {
+//             throw new Error("Download failed!");
+//         }
 
-        const blob = await response.blob(); 
-        const url = window.URL.createObjectURL(blob);
+//         const blob = await response.blob(); 
+//         const url = window.URL.createObjectURL(blob);
 
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "Resume__Rajarshi_Nath.pdf"; //filename which it will be download as
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
+//         const a = document.createElement("a");
+//         a.href = url;
+//         a.download = "Resume__Rajarshi_Nath.pdf"; //filename which it will be download as
+//         document.body.appendChild(a);
+//         a.click();
+//         a.remove();
 
-        window.URL.revokeObjectURL(url);
-    } catch (err) {
-        console.error("Error during download:", err);
-        alert("Couldn't download the file. Try again later.");
-    }
-};
+//         window.URL.revokeObjectURL(url);
+//     } catch (err) {
+//         console.error("Error during download:", err);
+//         alert("Couldn't download the file. Try again later.");
+//     }
+// };
